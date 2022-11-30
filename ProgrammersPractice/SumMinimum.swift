785ebdf092b1f2ea48b89957cb5f6312fae6cd28
RaeHoon Baek <bmp117@naver.com>
2022-12-01 00:05:27 +0900

    프로그래머스 코딩테스트 연습 - 각 함수명 및 .swift 파일 정리

--- ProgrammersPractice/SumMinimum.swift
+++ ProgrammersPractice/SumMinimum.swift
@@ -7,23 +7,13 @@
 
 import Foundation
 
-func solution3_1(_ A: [Int], _ B: [Int]) -> Int {
+func sumMinimum(_ A: [Int], _ B: [Int]) -> Int {
     var ans = 0
-    var A1 = A
-    var B1 = B
+    let A1 = A.sorted()
+    let B1 = B.sorted(by: >)
     
-    // [실행] 버튼을 누르면 출력값을 볼 수 있습니다.
-    print("Hello Swift")
-    
-    for _ in 0..<A.count {
-        guard let a = A1.min() else { return 0 }
-//        guard let a1 = A.firstIndex(of: a) else { return 0 }
-        A1.remove(at: a)
-        guard let b = B1.max() else { return 0 }
-//        guard let b1 = B.firstIndex(of: b) else { return 0 }
-        B1.remove(at: b)
-        ans += a * b
-        
+    for i in 0..<A1.count {
+        ans += A1[i] * B1[i]
     }
     return ans
 }
